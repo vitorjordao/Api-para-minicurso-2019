fc44f9b65d1a428722f17aee1dbd3f53d6e3348d
Vitor Jordão <srvitorlol@gmail.com>
2019-10-09 20:57:27 -0300

    Remove 'CORS'

---
+++ src/main/java/com/minicursoapi/core/configs/WebConfiguration.java
@@ -0,0 +1,15 @@
+package com.minicursoapi.core.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer{
+
+	@Override
+    public void addCorsMappings(final CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("*");
+    }
+}
